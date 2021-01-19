7b97e4925e5592f311b0472ccce2aaa82432502b
alyssaurbanski23 <831554@mystma.org>
2021-01-19 16:15:50 +0000

    circles try 2

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def sumBetween(x, y):
 
     total = 0
 
-    for i in range(x + 1, y):
+    for i in range (x + 1, y):
         total = total + i
 
     return total
 
-print(sumBetween(5,7))
+print (sumBetween(5,7))
